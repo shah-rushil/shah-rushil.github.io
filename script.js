07b7e638bc1170d7b046f6df5fd488bd3e17a57c
rubrub314 <rushilnshah2@gmail.com>
2024-05-11 17:46:14 -0400

    more bug fixes and added project pictures

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 const stylesheet = document.getElementById('stylesheet');
 const links = document.querySelectorAll('a[href^="#"]');
+const is_dark_mode = localStorage.getItem('dark-mode');
+
+if(is_dark_mode == 'on'){
+  darkModeToggle.textContent = "\u263D";
+  document.body.classList.toggle('dark-mode');
+}
 
 darkModeToggle.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
-  
   if(darkModeToggle.textContent == "\u263D"){
     darkModeToggle.textContent = '\u2600';
+    localStorage.setItem('dark-mode', 'off');
   }
   else{
     darkModeToggle.textContent = "\u263D";
+    localStorage.setItem('dark-mode', 'on');
   }
 });
 
